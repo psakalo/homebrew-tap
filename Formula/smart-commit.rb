9760dc429f47d1994548b208f049a6f7a05d695c
Pavlo Sakalo <pavlo.sakalo@gmail.com>
2024-02-03 10:03:08 -0500

    add url

--- Formula/smart-commit.rb
+++ Formula/smart-commit.rb
@@ -1,6 +1,8 @@
 class SmartCommit < Formula
   desc "smart-commit is a tool to help you write commit messages using OpenAI API"
   homepage "https://github.com/psakalo/smart-commit"
+  url "https://github.com/psakalo/smart-commit/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "" 
 
   depends_on "rust" => :build
 
